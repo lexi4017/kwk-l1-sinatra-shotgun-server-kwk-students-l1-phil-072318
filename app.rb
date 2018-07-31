61409029620016158d363cf9fd79ec8f9ccef472
Alexsia Light <elightl729@comcast.net>
2018-07-31 15:18:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -3,7 +3,7 @@ require_relative 'config/environment'
 class App < Sinatra::Base
 
   get '/' do 
-    "Welcome to your app!!!! Your soooooooo awesome but today is not your day"
+    "Your a loser and I dont understand!
   end
 
 end
